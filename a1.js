5ad5968a4e20261617eff319d1ced829f8377fd7
sadrda <kofler_a@yahoo.de>
2020-12-01 13:23:32 +0100

    day1

---
+++ a1.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./a1.txt", "utf8");
+
+const nums = input.split("\n").map((entry) => Number(entry));
+
+nums.forEach((firstNumber) => {
+  nums.forEach((secondNumber) => {
+    nums.forEach((thirdNumber) => {
+      if (
+        firstNumber !== secondNumber &&
+        secondNumber !== thirdNumber &&
+        thirdNumber !== firstNumber &&
+        firstNumber + secondNumber + thirdNumber === 2020
+      )
+        console.log(firstNumber * secondNumber * thirdNumber);
+    });
+  });
+});
---
+++ template.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+
+const input = fs.readFileSync("./a1.txt", "utf8");
